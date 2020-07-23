e00d044aa05090f224a6c8c163110f3fec1ae462
min-j <minj@bxscience.edu>
2020-07-23 18:06:28 +0000

    easier use

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def test():
     if request.method == "GET":
         return render_template("test.html")
     else:
-        print(request.form)
         t = request.form["term"]
         loc = request.form["loc"]
         find = search(t, loc)
@@ -25,10 +24,12 @@ def test():
         print(res)
         params = {
             'name': res['name'],
+            'price': res['price'],
             'img1': res['photos'][0],
             'img2': res['photos'][1],
             'img3': res['photos'][2],
-            'rating': getRating(res['rating'])
+            'rating': getRating(res['rating']),
+            'address': res['location']['display_address'][0] + ", " + res['location']['display_address'][1]
         }
         print(params['rating'])
         return render_template("test.html", **params)
