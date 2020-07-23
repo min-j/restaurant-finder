ed640059d6815941414c3df97c1438b9ef77f924
min-j <minj@bxscience.edu>
2020-07-23 17:52:48 +0000

    working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-from model import search
-# import os
+from model import search, getDetails, getRating
+
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -11,13 +11,24 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route('/test', methods= ["GET", "POST"])
 def test():
     if request.method == "GET":
-        return render_template("results.html")
+        return render_template("test.html")
     else:
         print(request.form)
-        t = request.form["Term"]
+        t = request.form["term"]
         loc = request.form["loc"]
-        print(search(t, loc))
-        return "POSTING..."
+        find = search(t, loc)
+        res = getDetails(find['businesses'][0]['id'])
+        print(res)
+        params = {
+            'name': res['name'],
+            'img1': res['photos'][0],
+            'img2': res['photos'][1],
+            'img3': res['photos'][2],
+            'rating': getRating(res['rating'])
+        }
+        print(params['rating'])
+        return render_template("test.html", **params)
--- model.py
+++ model.py
@@ -1,21 +1,40 @@
-# import os
+import os
+from dotenv import load_dotenv
 import requests
 
-# term = 'Chinese'
-# location = 'NYC'
-# SEARCH_LIMIT = 1
-url = 'https://api.yelp.com/v3/businesses/search'
+load_dotenv()
+API_KEY = os.getenv("KEY")
+headers = {
+    'Authorization': 'Bearer %s' % API_KEY
+}
 
-# 'Authorization': 'Bearer {}'.format(API_KEY),
-headers = { 
-    'Authorization': 'Bearer %s' % "Lj3NHxQYfUhoZ6-ZAHHBufMv_dCKumqgLhbtCe_uTyfKZIX7AotRbHZxdz9KFHNz10Aq7IREZvXVgR2LyWElUshB-uh2TXjtc3L_dC1uo6HxoolnHmwxzwjgcG4YX3Yx" 
+rating = {
+    0: "static/images/regular_0@3x.png",
+    1.5: "static/images/regular_1_half@3x.png",
+    2: "static/images/regular_2@3x.png",
+    2.5: "static/images/regular_2_half@3x.png",
+    3: "static/images/regular_3@3x.png",
+    3.5: "static/images/regular_3_half@3x.png",
+    4: "static/images/regular_4@3x.png",
+    4.5: "static/images/regular_4_half@3x.png",
+    5: "static/images/regular_5@3x.png"
 }
 
 
 def search(term, location, limit=1):
+    url = 'https://api.yelp.com/v3/businesses/search'
     url_params = {
-    'term': term.replace(' ', '+'),
-    'location': location.replace(' ', '+'),
-    'limit': limit
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': limit
     }
     return requests.get(url, headers=headers, params=url_params).json()
+
+
+def getDetails(iden):
+    url = 'https://api.yelp.com/v3/businesses/' + str(iden)
+    return requests.get(url, headers=headers).json()
+
+
+def getRating(num):
+    return rating[num]
