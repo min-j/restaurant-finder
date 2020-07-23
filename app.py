c5084cff8153faadaeff124f1dadffdd6ed48dda
SwanyeeAung <aungs@bxscience.edu>
2020-07-23 18:09:44 +0000

    main page ish

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from datetime import datetime
 # -- Initialization section --
 app = Flask(__name__)
 
@@ -7,4 +7,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", time=datetime.now())
