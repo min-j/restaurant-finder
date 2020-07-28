e40ccdffabd1dd3fc39f7e8776c5f5e759000d69
min-j <minj@bxscience.edu>
2020-07-28 15:13:02 +0000

    changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from model import search, getDetails, getRating
+import model as m
 import os
 from dotenv import load_dotenv
 
@@ -19,14 +19,11 @@ def index():
 @app.route('/', methods=["GET", "POST"])
 @app.route('/result', methods=["GET", "POST"])
 def result():
+    # the use of the restaurant finder twice is repetitive change it
+    # by searching for it depending on something
     if request.method == "GET":
-        return render_template("result.html")
-    else:
-        t = request.form["term"]
-        loc = request.form["loc"]
-        find = search(t, loc)
-        res = getDetails(find['businesses'][0]['id'])
-        print(res)
+        find = m.search(m.getRandom(), 'NYC')
+        res = m.getDetails(find['businesses'][0]['id'])
         try:
             price = "(" + res["price"] + ")"
         except (KeyError):
@@ -37,10 +34,40 @@ def result():
             'img1': res['photos'][0],
             'img2': res['photos'][1],
             'img3': res['photos'][2],
-            'rating': getRating(res['rating']),
+            'rating': m.getRating(res['rating']),
             'address': res['location']['display_address'][0] + ", " + res['location']['display_address'][1],
             'categories': res['categories'],
             'KEY': API_KEY
         }
-        print(params['categories'])
+        return render_template("result.html", **params)
+    else:
+        # when the user chooses more than choice throw it into a list
+        # and then pop it for viewing
+        if request.form["formType"] == "initial":
+            return "INITIAL FORM USED"
+        elif request.form["formType"] == "no":
+            return "NO"
+        else:
+            return "YES"
+        t = request.form["term"]
+        loc = request.form["loc"]
+        find = m.search(t, loc)  # Look for more than one business and adapt
+        res = m.getDetails(find['businesses'][0]['id'])
+        # print(res)
+        try:
+            price = "(" + res["price"] + ")"
+        except (KeyError):
+            price = ""
+        params = {
+            'name': res['name'],
+            'price': price,
+            'img1': res['photos'][0],
+            'img2': res['photos'][1],
+            'img3': res['photos'][2],
+            'rating': m.getRating(res['rating']),
+            'address': res['location']['display_address'][0] + ", " + res['location']['display_address'][1],
+            'categories': res['categories'],
+            'KEY': API_KEY
+        }
+        # print(params['categories'])
         return render_template("result.html", **params)
